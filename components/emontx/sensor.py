fe751865936222f5b382d10f36990bbbf37f20ee
magnayn <nigel.magnay@nirima.com>
2025-10-20 19:45:58 +0100

    Signed-off-by: magnayn <nigel.magnay@nirima.com>

--- components/emontx/sensor.py
+++ components/emontx/sensor.py
@@ -22,6 +22,6 @@ CONFIG_SCHEMA = (
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
