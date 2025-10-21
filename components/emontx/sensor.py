a6d45b567728e599f1b9a8e1fc3d819c21a8f9fd
magnayn <nigel.magnay@nirima.com>
2025-10-21 12:23:15 +0100

    Signed-off-by: magnayn <nigel.magnay@nirima.com>a

--- components/emontx/sensor.py
+++ components/emontx/sensor.py
@@ -5,6 +5,10 @@ from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ["uart"]
 
+CONF_UART_ID = "uart_id"
+CONF_VOLTAGE_SENSOR = "voltage_sensor"
+
+
 emontx_ns = cg.esphome_ns.namespace("emontx")
 EmontxSensor = emontx_ns.class_("EmontxSensor", cg.PollingComponent, uart.UARTDevice)
 
@@ -12,7 +16,9 @@ CONFIG_SCHEMA = (
     cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(EmontxSensor),
+            cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
             cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
+            cv.Optional(CONF_VOLTAGE_SENSOR): cv.use_id(sensor.Sensor),
         }
     )
     .extend(uart.UART_DEVICE_SCHEMA)
@@ -22,3 +28,7 @@ async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
+
+    if CONF_VOLTAGE_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_VOLTAGE_SENSOR])
+        cg.add(var.set_voltage_sensor(sens))
