373090d1834bf811fe5fb2aea9ac7d279d2659a5
magnayn <nigel.magnay@nirima.com>
2025-10-21 11:11:13 +0100

    Signed-off-by: magnayn <nigel.magnay@nirima.com>

--- components/emontx/sensor.py
+++ components/emontx/sensor.py
@@ -3,25 +3,22 @@ import esphome.config_validation as cv
 from esphome.components import sensor, uart
 from esphome.const import CONF_ID, CONF_UPDATE_INTERVAL
 
-DEPENDENCIES = ['uart']
-
-CONF_UART_ID = "uart_id"
+DEPENDENCIES = ["uart"]
 
 emontx_ns = cg.esphome_ns.namespace("emontx")
 EmontxSensor = emontx_ns.class_("EmontxSensor", cg.PollingComponent, uart.UARTDevice)
 
 CONFIG_SCHEMA = (
-    sensor.sensor_schema(unit_of_measurement="V", icon="mdi:flash")
-    .extend(
+    cv.Schema(
         {
             cv.GenerateID(): cv.declare_id(EmontxSensor),
-            cv.Required(CONF_UART_ID): cv.use_id(uart.UARTComponent),
             cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
         }
     )
+    .extend(uart.UART_DEVICE_SCHEMA)
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await uart.register_uart_device(var, config)
